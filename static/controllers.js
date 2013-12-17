57df4bfdfe776295de64da5bff2ff064e4a80fa9
Tim King <kingtimm@gmail.com>
2013-12-17 16:27:23 -0600

    doc updates

--- static/controllers.js
+++ static/controllers.js
@@ -1,19 +1,11 @@
 angular.module('whichLunchApp.controllers', []).
     controller('LunchCtrl', ['$scope', 'LunchPlaces', '$location', '$http',
         function($scope, LunchPlaces, $location) {
-            // Instantiate an object to store your scope data in (Best Practices)
             $scope.data = {};
             $scope.newPlaceText = '';
             var items = [];
 
             var listPlaces = function(etag) {
-//                response = LunchPlaces.list(function(response) {
-//                    // Assign the response INSIDE the callback
-//                    $scope.data.places = response;
-//
-//                    $scope.randomPlace = response[Math.floor(Math.random() * response.length)];
-//                });
-
                 LunchPlaces.list(etag).
                     success(function(data, status, headers, config){
                         items = angular.fromJson(data._items);
@@ -34,14 +26,17 @@ angular.module('whichLunchApp.controllers', []).
             $scope.addPlace = function() {
                 LunchPlaces.add($scope.newPlaceText).
                     success(function(data, status, headers, config) {
+
                         listPlaces();
                     });
                 $scope.newPlaceText = '';
             };
 
             $scope.removePlace = function(place) {
+                // eve requires etag to be passed in for deletion (versioning)
                 LunchPlaces.destroy(place._id, place.etag).
                     success(function(data, status, headers, config) {
+                        // eve caches the list of items unless an "If-None-Match" header is passed w/ etag
                         listPlaces(place.etag);
                     });
                 $scope.newPlaceText = '';
@@ -50,16 +45,11 @@ angular.module('whichLunchApp.controllers', []).
 
             $scope.tryAgain = function() {
                 var noRepeatList = items.slice();
-
                 var index = noRepeatList.indexOf($scope.randomPlace);
-
                 if (index > -1) {
                     noRepeatList.splice(index, 1);
                 }
 
                 $scope.randomPlace = noRepeatList[Math.floor(Math.random() * noRepeatList.length)]
             }
-
-        }])
-        .controller('MyCtrl2', [function() {
         }]);
